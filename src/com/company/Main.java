e78800e6831855d4b69828a8db72d3df80f43d8e
mstomel <jajalope372@gmail.com>
2018-02-28 08:50:24 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        FileReader a = new FileReader("Rivalries.txt");
+
+    }
+}
