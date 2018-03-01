9c6f6685d49479f0e8f8606034d81217690d2ae5
mstomel <jajalope372@gmail.com>
2018-03-01 08:50:56 -0500

    v0.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,31 @@
 package com.company;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
+    public static ArrayList<String> rivalries = new ArrayList<>();
     public static void main(String[] args) throws IOException {
-        FileReader a = new FileReader("Rivalries.txt");
 
+        File file = new File("Rivalries");
+        ArrayList<Team> teams = new ArrayList<>();
+
+        Scanner fileReader = new Scanner(file);
+        while(fileReader.hasNextLine()) {
+        rivalries.add(fileReader.nextLine());
+        }
+
+        file = new File("DataFiles/TeamSpellings.csv");
+        fileReader = new Scanner(file);
+        while(fileReader.hasNextLine()) {
+            String [] nameID = fileReader.nextLine().split(",");
+            Team temp = new Team(nameID[0], nameID[1]);
+            teams.add(temp);
+        }
+
+        System.out.println(teams);
     }
 }
---
+++ src/com/company/Team.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Team {
+
+    public Team(String name, String id){
+
+    }
+}
