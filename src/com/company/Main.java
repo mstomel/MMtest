08d340eefffabc70e8f8bdcab336cb9c9d0f1085
jajalope <jajalope372@gmail.com>
2018-03-11 21:12:49 -0400

    v3.0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -48,7 +48,7 @@ public class Main {
         Scanner mode = new Scanner(System.in);
         int mo = mode.nextInt();
         if (mo == 1) {
-            //to be implemented
+            newRunner();
         }
         else if (mo == 2) {
             oldRunner();
@@ -126,7 +126,7 @@ public class Main {
         return sTeams;
     }
     private static void recordBuilder(String year) throws IOException {
-        File file = new File("DataFiles/NCAATourneyCompactResults.csv");
+        File file = new File("PrelimData2018/RegularSeasonCompactResults_Prelim2018.csv");
         Scanner fileReader = new Scanner(file);
         while (fileReader.hasNextLine()) {
             String [] temp = fileReader.nextLine().split(",");
@@ -460,6 +460,151 @@ public class Main {
         System.out.println("enter year");
         String year = mode.next();
         r64 = seasonReader(year);
+        recordBuilder(year);
+
+        //round of 64
+        System.out.println("round of 64 matchups:");
+        for (int i = 0; i < 64; i++) {
+            if (i%2==0) {
+                System.out.print(r64.get(i).schoolName + " vs ");
+            }
+            else {
+                System.out.println(r64.get(i).schoolName);
+            }
+        }
+        for (int i = 0; i < 64; i+=2){
+            if ((r64.get(i).wins-r64.get(i).losses)>(r64.get(i+1).wins-r64.get(i+1).losses)) {
+                r32.add(r64.get(i));
+            }
+            else {
+                r32.add(r64.get(i+1));
+            }
+        }
+
+        //round of 32 predictor
+        System.out.println();
+        System.out.println("round of 32 matchups:");
+        for (int i = 0; i < 32; i++) {
+            if (i%2==0) {
+                System.out.print(r32.get(i).schoolName + " vs ");
+            }
+            else {
+                System.out.println(r32.get(i).schoolName);
+            }
+        }
+        for (int i = 0; i < 32; i+=2){
+            if ((r32.get(i).wins-r32.get(i).losses)>(r32.get(i+1).wins-r32.get(i+1).losses)) {
+                s16.add(r32.get(i));
+            }
+            else {
+                s16.add(r32.get(i+1));
+            }
+        }
+
+        //round of 16 predictor
+        System.out.println();
+        System.out.println("sweet 16 matchups:");
+        for (int i = 0; i < 16; i++) {
+            if (i%2==0) {
+                System.out.print(s16.get(i).schoolName + " vs ");
+            }
+            else {
+                System.out.println(s16.get(i).schoolName);
+            }
+        }
+        for (int i = 0; i < 16; i+=2){
+            if ((s16.get(i).wins-s16.get(i).losses)>(s16.get(i+1).wins-s16.get(i+1).losses)) {
+                e8.add(s16.get(i));
+            }
+            else {
+                e8.add(s16.get(i+1));
+            }
+        }
+
+        //elite 8 predictor
+        System.out.println();
+        System.out.println("elite eight matchups:");
+        for (int i = 0; i < 8; i++) {
+            if (i%2==0) {
+                System.out.print(e8.get(i).schoolName + " vs ");
+            }
+            else {
+                System.out.println(e8.get(i).schoolName);
+            }
+        }
+        for (int i = 0; i < 8; i+=2){
+            if ((e8.get(i).wins-e8.get(i).losses)>(e8.get(i+1).wins-e8.get(i+1).losses)) {
+                f4.add(e8.get(i));
+            }
+            else {
+                f4.add(e8.get(i+1));
+            }
+        }
+
+        //final 4 predictor
+        System.out.println();
+        System.out.println("final four matchups:");
+        for (int i = 0; i < 4; i++) {
+            if (i%2==0) {
+                System.out.print(f4.get(i).schoolName + " vs ");
+            }
+            else {
+                System.out.println(f4.get(i).schoolName);
+            }
+        }
+        for (int i = 0; i < 4; i+=2){
+            if ((f4.get(i).wins-f4.get(i).losses)>(r32.get(i+1).wins-r32.get(i+1).losses)) {
+                c.add(f4.get(i));
+            }
+            else {
+                c.add(f4.get(i+1));
+            }
+        }
+
+        //champ predictor
+        System.out.println();
+        System.out.println("championship game:");
+        for (int i = 0; i < 2; i++) {
+            if (i % 2 == 0) {
+                System.out.print(c.get(i).schoolName + " vs ");
+            } else {
+                System.out.println(c.get(i).schoolName);
+            }
+        }
+        for (int i = 0; i < 2; i+=2){
+            if ((c.get(i).wins-c.get(i).losses)>(c.get(i+1).wins-c.get(i+1).losses)) {
+                System.out.println("champion is: " + c.get(i).schoolName);
+            }
+            else {
+                System.out.println("champion is: " + c.get(i+1).schoolName);
+            }
+        }
+    }
+
+    private static ArrayList<Team> seasonReader18() throws IOException {
+        File file = new File("2018Matchups.csv");
+        Scanner fileReader = new Scanner(file);
+        ArrayList<Team> sTeams = new ArrayList<>();
+        while(fileReader.hasNextLine() ) {
+            String [] temp = fileReader.nextLine().split(",");
+            if (temp[0].equalsIgnoreCase("East")||temp[0].equalsIgnoreCase("South")||temp[0].equalsIgnoreCase("West")||temp[0].equalsIgnoreCase("Midwest")) {
+                continue;
+            }
+            else if (temp[2].equalsIgnoreCase("x")) {
+                Team Temp = new Team("x","0");
+                sTeams.add(Temp);
+            }
+            else {
+                sTeams.add(teamFinder(Integer.parseInt(temp[2])));
+            }
+        }
+        return sTeams;
+    }
+    private static void newRunner() throws IOException {
+        Scanner mode = new Scanner(System.in);
+        System.out.println("enter year");
+        String year = mode.next();
+        r64 = seasonReader18();
         recordBuilder(year);
 
         //round of 64
